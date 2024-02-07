e9a488a38fa0cbecc2910940d888a98c7ed0dde6
Christopher Schretzmann <cschretz@gmail.com>
2024-02-07 00:37:20 -0500

    cleaner test file

--- TestPokemonCardGame.java
+++ TestPokemonCardGame.java
@@ -1,11 +1,11 @@
 public class TestPokemonCardGame {
 
     public static void main(String[] args){
-       //MonteCarloSimulation test = new MonteCarloSimulation();
-      // test.runMonte();
         PokemonCardGame game = new PokemonCardGame();
         game.setupGame();
         game.runGame1();
+
+
 
     
     }
